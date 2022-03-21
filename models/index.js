0a0bb2cc2d962cad4aef6f89edaf82f61103d122
hannah AB <hannah.allisonbatt@gmail.com>
2022-03-21 21:16:53 +0800

    models - validation - relationships

--- models/index.js
+++ models/index.js
@@ -1,4 +1,10 @@
 const Mentor = require('./mentor');
 const Student = require('./student');
+const StudentAppointments = require('./studentAppointments');
+const Appointment = require('./appointment');
+const Langmentor = require('./lang_mentor');
+const LangStudent = require('./lang_student');
 
-module.exports = { Mentor, Student };
+
+
+module.exports = { Mentor, Student, StudentAppointments, Appointment, LangStudent, Langmentor };
