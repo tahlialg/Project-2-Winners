2f4f032ddbd417bc6c43f00902545a4818c153bf
danielnels <danieln@newground.net.au>
2022-03-27 14:05:27 +0800

    Udated dashboards and calender

--- public/js/loginMentor.js
+++ public/js/loginMentor.js
@@ -1,4 +1,5 @@
 async function loginFormHandler(event) {
+  event.preventDefault();
   const email = document.querySelector('#email-login').value.trim();
   const password = document.querySelector('#password-login').value.trim();
 
